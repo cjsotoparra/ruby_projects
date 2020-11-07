44f9ea7efc6f604d7140809db07eb9db32038987
cjsotoparra <cjsotoparra@email.arizona.edu>
2020-11-07 05:49:09 -0700

    finished part 1 of strings

---
+++ intro/working_with_strings.rb
@@ -0,0 +1,16 @@
+#string concatenation
+first_name = "christian"
+last_name = "soto"
+
+#p first_name + " " + last_name
+
+#sting interpolation (only works with double ""  and not ''
+#puts "My first name is #{first_name} and my last name is #{last_name}"
+#puts 'My first name is #{first_name} and my last name is #{last_name}'
+
+#Methods, how to find them and Common methods
+# puts first_name.reverse
+# puts first_name.capitalize
+# puts first_name.empty?
+# puts first_name.methods
+
