119ae14d8a7b3e0e60cc44b45cb5b53c8839f4c5
cjsotoparra <cjsotoparra@email.arizona.edu>
2020-11-12 02:45:39 -0700

    Cleaned up module

--- intro/password.rb
+++ intro/password.rb
@@ -1,23 +1,15 @@
 module Password
 	require 'bcrypt'
 
-#	users = [
-#		{ username: "chris", password: "password1" },
-#		{ username: "jack", password: "password2" },
-#		{ username: "arya", password: "password3" },
-#		{ username: "jonsnow", password:"password4" },
-#		{ username: "zach", password:"password5" }
-#	]
-
-	def Password.create_hash_digest(password)
+	def self.create_hash_digest(password)
 		BCrypt::Password.create(password)
 	end
 
-	def Password.verify_hash_digest(password)
+	def self.verify_hash_digest(password)
 		BCrypt::Password.new(password)
 	end
 
-	def Password.create_secure_users(users)
+	def self.create_secure_users(users)
 		users.each do |user_record|
 			user_record[:password] = create_hash_digest(user_record[:password])
 		end
@@ -25,7 +17,7 @@ module Password
 		return users
 	end
 
-	def Password.authenticate_user(username, password, list_of_users)
+	def self.authenticate_user(username, password, list_of_users)
 		list_of_users.each do |user_record|
 				if (user_record[:username] == username) && (verify_hash_digest(user_record[:password]) == password)
 					return user_record
