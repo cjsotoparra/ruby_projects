4a84252d9849ea7e62c2ec841e60eaa7bbd83d9f
cjsotoparra <cjsotoparra@email.arizona.edu>
2020-11-12 00:08:12 -0700

    Finished Student object

--- intro/student.rb
+++ intro/student.rb
@@ -1,14 +1,25 @@
 class Student
+
+	attr_accessor :first_name, :last_name, :email, :username
+
 	@first_name
 	@last_name
 	@email
 	@username
 	@password
 
+	def initialize(firstname, lname, email, username, password)
+		@first_name = firstname
+		@last_name = lname
+		@email = email
+		@username = username
+		@password = password
+	end
+
 	def to_s
-		"First name: #{@first_name}"
-	end 
+		"First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, email address #{@email}"
+	end
 end
 
-christ = Student.new
+christ = Student.new("Christ", "Some last name", "asd@example.com", "someName", "password1")
 puts christ
